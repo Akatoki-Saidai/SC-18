293fa1504b8f553adca0da122e15141747885918
tomoyuki-miyauchi <105530032+tomoyuki-miyauchi@users.noreply.github.com>
2023-07-07 20:01:38 +0900

    Update BBM1.ino

--- BBM/BBM1.ino
+++ BBM/BBM1.ino
@@ -1,4 +1,20 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+Adafruit_BME280 bme;
+
+float temp;
+float pressure;
+float humid;
+
 void setup(){
+  Serial.begin(115200);
+  bool status;
+  status = bme.begin(0x76);  
+  while (!status) {
+    Serial.println("BME280 sensorが使えません");
+    delay(1000);
+  }
 
 }
 
