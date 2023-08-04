75932a09f0892b2b267d34413f81adbb26317a4e
yosshii8888 <yoyoyosshii2@gmail.com>
2023-08-04 16:11:17 +0900

    Update Serial.py

--- Serial.py
+++ Serial.py
@@ -9,7 +9,7 @@ if True:
 while True:
     ser.write(byte_camera_order)
     rundata = ser.readline().decode("utf-8").rstrip()
-    #with open("/home/nbus/N-BUS_RunReport.txt", "a") as f:
-        #f.write(rundata + "\n")
+    with open("/home/nbus/N-BUS_RunReport.txt", "a") as f:
+        f.write(rundata)
     print(rundata)
 ser.close()
