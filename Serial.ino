76854e047d7b3033742c86cf17dc1fae0e333240
yosshii8888 <yoyoyosshii2@gmail.com>
2023-08-04 15:17:07 +0900

    Serial_set
    UART2にしました。ラズパイ->ESPはまだです

--- Serial.ino
+++ Serial.ino
@@ -4,10 +4,28 @@ int RX_PIN = 22;
 int TX_PIN = 23;
 
 void setup() {
-  Serial1.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
+  Serial2.begin(19200, SERIAL_8N1, RX_PIN, TX_PIN);
 }
 
 void loop() {
-  Serial1.println("Hello Raspberry pi");
+
+  char byte_camera_order = Serial2.read();
+  Serial.println(camera_order);
+
+    /*
+    if (camera_order == 0){
+      ;
+    } //stoppage
+    elif (camera_order  == 1){
+      ;
+    } //forward
+    elif (camera_order == 2){
+      ;
+    } //right
+    elif (camera_order == 3){
+      ;
+    } //left*/
+
+  Serial2.println("Hello Raspberry pi");
   delay(100);
 }
--- Serial.py
+++ Serial.py
@@ -1,9 +1,15 @@
 import serial
 
-ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=10)
+ser = serial.Serial('/dev/ttyAMA0', 19200, timeout=10)
+
+if True:
+    camera_order = "Hello, ESP32" #ここに1~4を入れてESPに送信
+    byte_camera_order = camera_order.encode()
 
 while True:
+    ser.write(byte_camera_order)
     rundata = ser.readline().decode("utf-8").rstrip()
-    with open("/home/nbus/N-BUS_RunReport.txt", "a") as f:
-        f.write(rundata + "\n")
+    #with open("/home/nbus/N-BUS_RunReport.txt", "a") as f:
+        #f.write(rundata + "\n")
     print(rundata)
+ser.close()
