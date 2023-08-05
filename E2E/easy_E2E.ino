4634b9997455f5e78c4b17517206de4405506d93
tomoyuki-miyauchi <105530032+tomoyuki-miyauchi@users.noreply.github.com>
2023-08-05 11:33:56 +0900

    Update easy_E2E.ino

--- E2E/easy_E2E.ino
+++ E2E/easy_E2E.ino
@@ -7,27 +7,42 @@
 double heading;
 double declinationAngle;
 double headingDegrees;
-double Angle_gy270;
+double Angle_north;
 double Angle_Goal;
 double Angle_gps;
 double Angle_heading;
 double rrAngle, llAngle;
 double Sum_headingDegrees;
 
-double GOAL_lat = 35.860545000;
-double GOAL_lng = 139.606940001;
+double GOAL_lat = 35.86512;
+double GOAL_lng = 139.60740;
 
 double distance; //直進前後でゴールに近づいているかどうかを表す
 double Pre_distance;
-// variables___GY-271
 double heading_data;
 double heading_array[5];
 double heading_sum = 0;
 double omega;
 double azimuth;
 double delta_lng;
-double gps_latitude;
-double gps_longitude;
+double altitude0;
+int camera_order;
+int phase = 0;
+int phase_state = 0;
+int altitude_phasestate = 0; //地上から待機フェーズに移行するための変数
+
+//BME280
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+//1013.25は地球の海面上の大気圧の平均値(1気圧)です。
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BME280 bme;
+
+float temp;
+float pressure;
+float humid;
+float altitude;//高度
 
 
 //BNO055
@@ -40,32 +55,29 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 //GPS
 #include <TinyGPS++.h>
-#include <HardwareSerial.h>
+//#include <HardwareSerial.h>
 TinyGPSPlus gps;
-HardwareSerial SerialGPS(1);
+//HardwareSerial SerialGPS(1);
 //GPS
 
+double gps_latitude = gps.location.lat();
+double gps_longitude = gps.location.lng();
 
 //Raspberry Pi 通信
 #include <HardwareSerial.h>
 int RX_PIN = 22;
 int TX_PIN = 23;
 
-int phase = 0;
-int phase_state = 0;
 
 //bnicromewire
 int cutparac = 23;          //切り離し用トランジスタのピン番号の宣言
 int outputcutsecond = 3;    //切り離し時の9V電圧を流す時間，単位はsecond
-//enicromewire
-
-
 
 
 //motor
 void accel()
 {
-for (int i = 10; i < 240; i = i + 4)
+for (int i = 0; i < 240; i = i + 6)
   {
     ledcWrite(0, 0);
     ledcWrite(1, i);
@@ -77,7 +89,7 @@ for (int i = 10; i < 240; i = i + 4)
 //brake
 void brake()
 {
-  for (int i = 240; i > 0; i = i - 4)
+  for (int i = 240; i > 0; i = i - 6)
   {
     ledcWrite(0, 0);
     ledcWrite(1, i);
@@ -121,7 +133,7 @@ void rightturn()
 }
 
 //回転
-void rotating()
+void reverse_rotating()
 {
   ledcWrite(0, 0);
   ledcWrite(1, 140);
@@ -139,7 +151,7 @@ void slow_rotating()
 }
 
 //反回転
-void reverse_rotating()
+void rotating()
 {
   ledcWrite(0, 140);
   ledcWrite(1, 0);
@@ -166,31 +178,18 @@ double CalculateDis(double GOAL_lng, double GOAL_lat, double gps_longitude, doub
   return distance;
 }
 
-double desiredDistance = 0.0;
+double desiredDistance = 5.0;
 
 // 角度計算用の関数
-double CalculateAngle(double GOAL_lng, double GOAL_lat, double gps_longitude, double gps_latitude)
+double CalculateAngle(double GOAL_lng, double GOAL_lat, double gps_lng, double gps_lat)
 {
+  double degrees = (atan2((gps_lng - GOAL_lng) * 1.23, (gps_lat - GOAL_lat)) * 57.3 + 180);
+  return degrees;
+}
 
-  GOAL_lng = deg2rad(GOAL_lng);
-  GOAL_lat = deg2rad(GOAL_lat);
-
-  gps_longitude = deg2rad(gps_longitude);
-  gps_latitude = deg2rad(gps_latitude);
-
-  // 目標地点までの角度を導出
-  delta_lng = GOAL_lng - gps_longitude;
-  azimuth = rad2deg(atan2(sin(delta_lng), cos(gps_latitude) * tan(GOAL_lat) - sin(gps_latitude) * cos(delta_lng)));
-
-  if (azimuth < 0)// azimuthを0°～360°に収める
-  {
-    azimuth += 360;
-  }
-  else if (azimuth > 360)
-  {
-    azimuth -= 360;
-  }
-  return azimuth;
+double accelXYZ (double accelX,double accelY,double accelZ){
+  double E_pow_accelXYZ = sqrt((pow(accelX, 2) + pow(accelY, 2) + pow(accelZ, 2)));
+  return E_pow_accelXYZ;
 }
 
 
@@ -203,11 +202,19 @@ void setup(){
   //Raspberry Pi
   Serial2.begin(19200,SERIAL_8N1, RX_PIN, TX_PIN);
 
-//GPS
+  //GPS
   Serial1.begin(9600,SERIAL_8N1,5,18); 
-//GPS
 
-//BNO055
+    // BME280
+  bool status = bme.begin(0x76);
+  if (!status) {
+    Serial.println("BME280 sensorが使えません");
+    while (1);
+  }
+  altitude0 = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  Serial.println(altitude0);
+
+  //BNO055
   Wire.begin(21,22);
   if (!bno.begin()) {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
@@ -216,156 +223,175 @@ void setup(){
 
   delay(1000);
 
-//bmotor
+  //bmotor
   ledcSetup(0, 490, 8);
   ledcSetup(1, 490, 8);
   ledcSetup(2, 490, 8);
   ledcSetup(3, 490, 8);
-
   ledcAttachPin(32, 0);
   ledcAttachPin(33, 1);
   ledcAttachPin(26, 2);
   ledcAttachPin(25, 3);
-//motor
 
-
-
-//nicromewire
+  //nicromewire
   pinMode(cutparac, OUTPUT);      //切り離し用トランジスタの出力宣言
   digitalWrite(cutparac, LOW);    //切り離し用トランジスタの出力オフ
-
-
-//nicromewire
-
     
 }
 
 void loop() {
-    while (SerialGPS.available() > 0) {
-    char c = SerialGPS.read();
+  
+  while (Serial1.available() > 0) {
+    char c = Serial1.read();
     gps.encode(c);
     if (gps.location.isUpdated()){
+      //こっからスタート
 
-    // Add your GPS-related code here, using the 'gps' object
-  // BME280
-  float temp, pressure, humid, altitude; // Assuming these variables are defined in BME280 sensor code, declare them here
+      switch (phase){
+        case 0:
+        if(altitude_phasestate == 0){
+          altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+          Serial.print("altitude0 :");
+          Serial.println(altitude0);
+          Serial.print("altitude :");
+          Serial.println(altitude);
+          if(altitude > altitude0 + 2.0){
+            altitude_phasestate = 1;
+          }
+        }
+        if(altitude_phasestate == 1){
+          Serial.println("stand-by phase");
+          Serial.println("accelZ");
+          // BNO055からセンサーデータを取得
+          imu::Vector<3> accelermetor = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+          double accelZ = accelermetor.z();
+          double accelX = accelermetor.x();
+          double accelY = accelermetor.y();
+        
+          Serial.println(accelX);
+          Serial.println(accelY);
+          Serial.println(accelZ);
+          //Serial2.println(gps.time);
+          stoppage();
+          if(accelXYZ(accelX, accelY, accelZ) >= 15.0){
+            phase_state = 1;
+            altitude_phasestate = 2;
+            Serial.print("falling!");
+          }
+        }
+        if(phase_state == 1){
+          //高度測定
+          altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+          if(altitude < altitude0 + 2){
+            Serial.println("Go to long phase");
+            delay(5000);
+  
+            phase_state = 2;
+            //ニクロム線を切る
+            /*
+            delay(1000);
+            Serial.print("WARNING: 9v voltage on.\n");
+            digitalWrite(cutparac, HIGH); //オン
+            delay(outputcutsecond * 1000); //十秒間電流を流す
+            Serial.print("WARNING: 9v voltage off.\n");
+            digitalWrite(cutparac, LOW); //オフ
+            */
+            
+            //初期前進
+            accel();
+            delay(2000);
+            brake();
+            delay(2000);
+            phase = 1;
+          }
+        }
+        break;
 
-  //BNO055
-  // ジャイロセンサ値の取得と表示
-  imu::Vector<3> gyroscope = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  // 加速度センサ値の取得と表示
-  // BNO055からセンサーデータを取得
-  sensors_event_t event;
-  bno.getEvent(&event);
-
-  // Z軸の加速度を取得
-  float accelZ = event.acceleration.z;
-  // 磁力センサ値の取得と表示
-  imu::Vector<3> magnetmetor = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
-
+      
+        case 1:
+        // Add your GPS-related code here, using the 'gps' object
         gps_latitude = gps.location.lat();
         gps_longitude = gps.location.lng();
+      
+        Serial.print("LAT:");
+        Serial.println(gps.location.lat(), 9);
+        Serial.print("LONG:");
+        Serial.println(gps.location.lng(), 9);
+        Serial.flush();
+        //Serial2.println(gps_time);
+        Serial2.println("transition completed");
+        Serial2.print("LAT(PHASE4_START):");
+        Serial2.println(gps_latitude, 9);
+        Serial2.print("LONG(PHASE4_START):");
+        Serial2.println(gps_longitude, 9);
+        Serial2.flush();
 
 
-
-    while(accelZ <= 12.0) {
-      //Serial2.println(gps.time);
-      stoppage();
-      delay(50);
-    }
-/*
-    delay(1000);
-    Serial.print("WARNING: 9v voltage on.\n");
-    digitalWrite(cutparac, HIGH); //オン
-    delay(outputcutsecond * 1000); //十秒間電流を流す
-    Serial.print("WARNING: 9v voltage off.\n");
-    digitalWrite(cutparac, LOW); //オフ
-    */
-
-    accel();
-    delay(3000);
-    brake();
-    delay(3000);
-
-    delay(1000);
-      //Serial2.println(gps_time);
-      Serial2.println("transition completed");
-      Serial2.print("LAT(PHASE4_START):");
-      Serial2.println(gps_latitude, 9);
-      Serial2.print("LONG(PHASE4_START):");
-      Serial2.println(gps_longitude, 9);
-      Serial2.flush();
-
+        if(phase_state == 2){
+          gps_latitude = gps.location.lat();
+          gps_longitude = gps.location.lng();
           int CurrentDistance = CalculateDis(GOAL_lng, GOAL_lat, gps_longitude, gps_latitude);
-          Serial2.print("CurrentDistance=");
-          Serial2.println(CurrentDistance);
-
-          if (desiredDistance >= CurrentDistance)
-          {
+          Serial.print("CurrentDistance=");
+          Serial.println(CurrentDistance);
+          if (desiredDistance >= CurrentDistance){
             // カラーコーンとの距離が理想値よりも小さい場合は次のフェーズに移行する
-            break;
+            Serial.println("Go to short phase");
+            phase = 2;
+            phase_state = 3;
           }else{
+            Serial.println("Continue long_phase");
             delay(100);
             accel();
+            delay(2000);
+            Serial.print("LAT:");
+            Serial.println(gps.location.lat(), 9);
+            Serial.print("LONG:");
+            Serial.println(gps.location.lng(), 9);
+            Serial.flush();
             Serial2.print("LAT:");
-            Serial2.println(gps_latitude, 9);
+            Serial2.println(gps.location.lat(), 9);
             Serial2.print("LONG:");
-            Serial2.println(gps_longitude, 9);
+            Serial2.println(gps.location.lng(), 9);
             Serial2.flush();
             delay(3000);
-            forward();
+            brake();
+            Serial.print("LAT:");
+            Serial.println(gps.location.lat(), 9);
+            Serial.print("LONG:");
+            Serial.println(gps.location.lng(), 9);
+            Serial.flush();
             Serial2.print("LAT:");
-            Serial2.println(gps_latitude, 9);
+            Serial2.println(gps.location.lat(), 9);
             Serial2.print("LONG:");
-            Serial2.println(gps_longitude, 9);
+            Serial2.println(gps.location.lng(), 9);
             Serial2.flush();
             delay(1000);
             stoppage();
             // Goalまでの偏角を計算する
+            gps_latitude = gps.location.lat();
+            gps_longitude = gps.location.lng();
             Angle_Goal = CalculateAngle(GOAL_lng, GOAL_lat, gps_longitude, gps_latitude);
+            Serial.println("degrees calculationning");
+            Serial.print("Angle_Goal :");
+            Serial.println(Angle_Goal);
+            Serial.print("Angle_north :");
+            Serial.println(Angle_north);
 
-            Sum_headingDegrees = 0.0;
-              for (int i = 0; i < 15; i++){
-                delay(10);
-                heading = event.orientation.x;
-                declinationAngle = (-7.0 + (46.0 / 60.0)) / (180 / PI);
-                heading += declinationAngle;
-                if (heading < 0){
-                  heading += 2 * PI;
-                }
-                if (heading > 2 * PI){
-                  heading -= 2 * PI;
-                }
-
-                // Convert to degrees
-                headingDegrees = heading * 180 / M_PI;
-
-                if (headingDegrees < 0){
-                  headingDegrees += 360;
-                }
-
-                if (headingDegrees > 360){
-                  headingDegrees -= 360;
-                }
-                Sum_headingDegrees += headingDegrees;
-              }
-              Angle_gy270 = Sum_headingDegrees / 15;
-
-              // どちらに回ればいいか計算
-              rrAngle = -Angle_gy270 + Angle_Goal;
-              if (rrAngle < 0){
-                rrAngle += 360;
-              }
-              if (rrAngle > 360){
-                rrAngle -= 360;
-              }
-              llAngle = Angle_gy270 - Angle_Goal;
-              if (llAngle < 0){
-                llAngle += 360;
-              }
-              if (llAngle > 360){
-                llAngle -= 360;
-              }
+            // どちらに回ればいいか計算
+            rrAngle = -Angle_north + Angle_Goal;
+            if (rrAngle < 0){
+              rrAngle += 360;
+            }
+            if (rrAngle > 360){
+              rrAngle -= 360;
+            }
+            llAngle = Angle_north - Angle_Goal;
+            if (llAngle < 0){
+              llAngle += 360;
+            }
+            if (llAngle > 360){
+              llAngle -= 360;
+            }
 
             if (rrAngle > llAngle){
               //反時計回り
@@ -375,6 +401,7 @@ void loop() {
                 rotating();
                 delay(400);
                 stoppage();
+                delay(1000);
               }
             }else{
               //時計回り
@@ -384,15 +411,47 @@ void loop() {
                 reverse_rotating();
                 delay(400);
                 stoppage();
+                delay(1000);
               }
             }
           }
-        
-        break;
         }
+        break;
 
 
-
-        delay(1000000000);
+        case 2:
+          int kari = 0;
+      
+          if(kari < 1/*コーンの面積が閾値を超える*/){
+            //phase = 3; //finish
+            Serial.println("GOAL GOAL GOAL");
+          }else if(phase_state == 3){
+            Serial.println("Continue short phase");
+            camera_order = Serial2.parseInt();
+            Serial.print("camera_order :");
+            Serial.println(camera_order);
+             if (camera_order == 0){
+               stoppage();
+               delay(2000);
+             }else if (camera_order  == 1){
+               brake();
+               delay(2000);
+             }else if (camera_order == 2){
+               rotating();
+               delay(100);
+             }else if (camera_order == 3){
+               reverse_rotating();
+               delay(100);
+             }
+           }
+        break;
+        /*
+        case 3:
+          stoppage();
+          delay(10);
+        break;
+        */
+      }
+    }
   }
 }
