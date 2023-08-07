6fa3e82d810d97b8b87bae9bcbd85ea906fedf6c
yosshii8888 <yoyoyosshii2@gmail.com>
2023-08-07 12:22:47 +0900

    Update Serial.ino

--- Serial.ino
+++ Serial.ino
@@ -3,32 +3,27 @@
 int RX_PIN = 22;
 int TX_PIN = 23;
 
-void setup() {
-  Serial.begin(9600);
+void setup() {;
   Serial2.begin(19200, SERIAL_8N1, RX_PIN, TX_PIN);
-  Serial.println("on");
 }
 
 void loop() {
-  
-  int camera_order = Serial2.parseInt();
-  //int camera_order = Serial2.read();
-  Serial.println(camera_order);
-
-    /*
+  int camera_order = Serial2.read();
     if (camera_order == 0){
-      ;
-    } //stoppage
+      brake();止まる
+    }
     elif (camera_order  == 1){
-      ;
-    } //forward
+      forward();//前へ
+    }
     elif (camera_order == 2){
-      ;
-    } //right
+      leftturn();//右へ
+    }
     elif (camera_order == 3){
-      ;
-    } //left*/
+      rightturn();//左へ
+    else () {
+      slow_rotating();//探す
+    }
 
-  Serial2.println("Hello Raspberry pi");
-  delay(100);
+  //Serial2.println("Hello Raspberry pi");
+  //delay(100);
 }
